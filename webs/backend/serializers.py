f05d526cbeaffb5dea459e466ce0ed46c947b465
manulangat <emmanuelthedeveloper@gmail.com>
2020-06-28 23:15:22 +0300

    ft-adding_serializers-backend

---
+++ webs/backend/serializers.py
@@ -0,0 +1,26 @@
+# from rest_framework import serializers
+
+
+# from .models import Articles,Records
+
+# class ArticleSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Articles
+#         fields = (
+#             'id',
+#             'title',
+#             'post',
+#             'user',
+#             'comments',
+#             'scrap_date'
+#         )
+
+# class RecordsSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Articles
+#         fields = (
+#             'id',
+#             'start_time',
+#             'success',
+#             'scrap_date'
+#         )
