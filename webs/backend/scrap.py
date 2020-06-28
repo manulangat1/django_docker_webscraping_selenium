67405e276818cd74a62e323f76d54e6564754655
manulangat <emmanuelthedeveloper@gmail.com>
2020-06-28 22:38:37 +0300

    ft-adding_logging_into_dev_using_github-backend

--- webs/backend/scrap.py
+++ webs/backend/scrap.py
@@ -3,5 +3,27 @@ from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
 url = "https://dev.to/"
+from decouple import config 
+username = config('username')
+pw =config('pw')
 def scraps(url):
     print(url)
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(url)
+    sleep(2)
+    login = driver.find_element_by_xpath("//section[1]/div/a[1]").click()
+    sleep(17)
+    input1 = driver.find_element_by_xpath('//*[@id="login_field"]').send_keys(username)
+    sleep(3)
+    input2 = driver.find_element_by_xpath('//*[@id="password"]').send_keys(pw)
+    sleep(3)
+    sign = driver.find_element_by_name("commit").click()
+    sleep(15)
+    # login = driver.find_element_by_css_selector(".crayons-btn crayons-btn--l crayons-btn--brand-github crayons-btn--icon-left")
+    print(login)
+    sleep(20)
+    links = driver.find_elements_by_xpath('//*[@id="featured-story-marker"]/div/div[1]')
+    print(links)
+    driver.quit()
+
+scraps(url)
