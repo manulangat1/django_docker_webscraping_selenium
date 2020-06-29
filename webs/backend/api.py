9eeea07a8056e8720853efdd93e0644d04e48928
manulangat <emmanuelthedeveloper@gmail.com>
2020-06-29 11:56:48 +0300

    ch-migrating_the_database_backend_models-backend

--- webs/backend/api.py
+++ webs/backend/api.py
@@ -1,22 +1,22 @@
-# from rest_framework import generics
-# from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework.response import Response
 
-# from .models import Records,Articles
-# from .serializers import ArticleSerializer,RecordsSerializer
+from .models import Records,Articles
+from .serializers import ArticleSerializer,RecordsSerializer
 
 
-# class ArticleListView(generics.ListAPIView):
-#     serializer_class = ArticleSerializer
-#     queryset = Articles.objects.all()
+class ArticleListView(generics.ListAPIView):
+    serializer_class = ArticleSerializer
+    queryset = Articles.objects.all()
 
-# class ArticleDetailView(generics.RetrieveAPIView):
-#     serializer_class = ArticleSerializer
-#     queryset = Articles.objects.all()
+class ArticleDetailView(generics.RetrieveAPIView):
+    serializer_class = ArticleSerializer
+    queryset = Articles.objects.all()
 
-# class RecordListView(generics.ListAPIView):
-#     serializer_class = RecordsSerializer
-#     queryset = Records.objects.all()
+class RecordListView(generics.ListAPIView):
+    serializer_class = RecordsSerializer
+    queryset = Records.objects.all()
 
-# class RecordDetailView(generics.RetrieveAPIView):
-#     serializer_class = RecordsSerializer
-#     queryset = Records.objects.all()
+class RecordDetailView(generics.RetrieveAPIView):
+    serializer_class = RecordsSerializer
+    queryset = Records.objects.all()
